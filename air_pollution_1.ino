055630cc5dbff12ac32371efd3fc50c32bbcb3bc
vaidehi warghat <98640918+Vaidehi-w@users.noreply.github.com>
2023-07-02 17:26:52 +0530

    Add files via upload
    It is a air pollution detection system which detects the polluted air as poor air or fresh air

---
+++ air_pollution_1.ino
@@ -0,0 +1,61 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int pin8 = 8;
+int analogPin = A0;    
+int sensorValue = 0;     
+
+//function to set up default status
+void setup() {
+  pinMode(analogPin, INPUT);
+  pinMode(pin8, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.print("What is the air ");
+  lcd.print("quality today?");
+  Serial.begin(9600);
+  lcd.display();
+}
+
+//function that determines the quality of the air
+void loop() {
+  
+  delay(1000);
+  sensorValue = analogRead(analogPin);     
+  Serial.print("Air Quality in PPM = ");
+  Serial.println(sensorValue);          
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print ("Air Quality: ");
+  lcd.print (sensorValue);
+  
+  if (sensorValue<=100)
+   {
+   Serial.print("Fresh Air ");
+   Serial.print ("\r\n");
+   lcd.setCursor(0,1);
+   lcd.print("Fresh Air");
+   }
+  else if( sensorValue>=150 && sensorValue<=350 )
+   {
+   Serial.print("Poor Air");
+   Serial.print ("\r\n");
+   lcd.setCursor(0,1);
+   lcd.print("Poor Air");
+   }
+  else if (sensorValue>=350 )
+   {
+   Serial.print("Very Poor Air");
+   Serial.print ("\r\n");
+   lcd.setCursor(0,1);
+   lcd.print("Very Poor Air");
+   }
+  //to determine when the alarm sets off
+  if (sensorValue >450) {
+    digitalWrite(pin8, HIGH);
+  }
+  else {
+    digitalWrite(pin8, LOW);
+  }
+}
